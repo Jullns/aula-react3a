7b851d1b08d2bea6a19cfbf6efe9124f23d6937a
Jullia dos Santos <julliansantos30@gmail.com>
2026-09-16 12:11:49 +0000

    projeto base

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div id='container'>
+      <h1>Sejam bem vindos ao react</h1>
+    </div>
+  )
+}
